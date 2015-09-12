10a5e8696822de0c2fa24cd01b54da0c62c5cc53
Kocsis Máté <kocsismate@woohoolabs.com>
2015-09-12 22:24:49 +0200

    Fixed namespaces

--- tests/JsonApi/Exception/MediaTypeUnsupportedTest.php
+++ tests/JsonApi/Exception/MediaTypeUnsupportedTest.php
@@ -1,5 +1,5 @@
 <?php
-namespace WoohooLabsTest\Yin\JsonApi\Schema;
+namespace WoohooLabsTest\Yin\JsonApi\Exception;
 
 use PHPUnit_Framework_TestCase;
 use WoohooLabs\Yin\JsonApi\Exception\MediaTypeUnsupported;
