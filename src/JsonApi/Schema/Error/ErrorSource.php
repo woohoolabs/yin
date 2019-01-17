13a44588987d1f1cc798ec56b81f5ae6b3ba74f5
kocsismate <kocsismate@woohoolabs.com>
2019-01-17 15:02:28 +0100

    Deprecate Error and ErrorSource

---
+++ src/JsonApi/Schema/Error/ErrorSource.php
@@ -0,0 +1,58 @@
+<?php
+declare(strict_types=1);
+
+namespace WoohooLabs\Yin\JsonApi\Schema\Error;
+
+class ErrorSource
+{
+    /**
+     * @var string
+     */
+    private $pointer;
+
+    /**
+     * @var string
+     */
+    private $parameter;
+
+    public static function fromPointer(string $pointer): ErrorSource
+    {
+        return new self($pointer, "");
+    }
+
+    public static function fromParameter(string $parameter): ErrorSource
+    {
+        return new self("", $parameter);
+    }
+
+    public function __construct(string $pointer, string $parameter)
+    {
+        $this->pointer = $pointer;
+        $this->parameter = $parameter;
+    }
+
+    public function transform(): array
+    {
+        $content = [];
+
+        if ($this->getPointer()) {
+            $content["pointer"] = $this->getPointer();
+        }
+
+        if ($this->getParameter()) {
+            $content["parameter"] = $this->getParameter();
+        }
+
+        return $content;
+    }
+
+    public function getPointer(): string
+    {
+        return $this->pointer;
+    }
+
+    public function getParameter(): string
+    {
+        return $this->parameter;
+    }
+}
--- src/JsonApi/Schema/ErrorSource.php
+++ src/JsonApi/Schema/ErrorSource.php
@@ -3,56 +3,11 @@ declare(strict_types=1);
 
 namespace WoohooLabs\Yin\JsonApi\Schema;
 
-class ErrorSource
-{
-    /**
-     * @var string
-     */
-    private $pointer;
+use WoohooLabs\Yin\JsonApi\Schema\Error\ErrorSource;
 
-    /**
-     * @var string
-     */
-    private $parameter;
+class_alias(ErrorSource::class, 'WoohooLabs\Yin\JsonApi\Schema\ErrorSource');
 
-    public static function fromPointer(string $pointer): ErrorSource
-    {
-        return new self($pointer, "");
-    }
-
-    public static function fromParameter(string $parameter): ErrorSource
-    {
-        return new self("", $parameter);
-    }
-
-    public function __construct(string $pointer, string $parameter)
-    {
-        $this->pointer = $pointer;
-        $this->parameter = $parameter;
-    }
-
-    public function transform(): array
-    {
-        $content = [];
-
-        if ($this->getPointer()) {
-            $content["pointer"] = $this->getPointer();
-        }
-
-        if ($this->getParameter()) {
-            $content["parameter"] = $this->getParameter();
-        }
-
-        return $content;
-    }
-
-    public function getPointer(): string
-    {
-        return $this->pointer;
-    }
-
-    public function getParameter(): string
-    {
-        return $this->parameter;
-    }
-}
+trigger_error(
+    'Class WoohooLabs\Yin\JsonApi\Schema\ErrorSource is deprecated, use ' . ErrorSource::class . ' instead.',
+    E_USER_DEPRECATED
+);
