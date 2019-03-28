aa7f48d81c652704efa3eebdfdac4128316f10f8
Bruno Zoric <bruno.zoric@gmail.com>
2019-03-28 13:00:57 +0100

    Relationship starts with data key set as null

--- src/JsonApi/Schema/Relationship/AbstractRelationship.php
+++ src/JsonApi/Schema/Relationship/AbstractRelationship.php
@@ -131,12 +131,14 @@ abstract class AbstractRelationship
         array $defaultRelationships,
         array $additionalMeta = []
     ): ?array {
-        $relationship = null;
-
+        $relationship = [
+            "data" => null,
+        ];
+        
         if (
             (
                 $transformation->fetchedRelationship === $relationshipName &&
-                $this->data &&
+                $this->data !== null &&
                 $this->omitDataWhenNotIncluded === false
             ) ||
             $transformation->request->isIncludedRelationship(
@@ -151,7 +153,6 @@ abstract class AbstractRelationship
         }
 
         if ($transformation->request->isIncludedField($resourceType, $relationshipName)) {
-            $relationship = [];
 
             // Links
             if ($this->links !== null) {
